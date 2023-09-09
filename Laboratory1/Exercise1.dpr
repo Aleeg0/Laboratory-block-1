6cb8835594436621f5f11932bc9cbac73c337477
Aleeg0 <83497083+Aleeg0@users.noreply.github.com>
2023-09-09 23:05:37 +0300

    Add files via upload

---
+++ Laboratory1/Exercise1.dpr
@@ -0,0 +1,61 @@
+Program Exercise1;
+
+Uses
+    System.SysUtils, Math;
+
+// initialization
+Var
+    Num1: Real = 0;
+    Num2: Real = 0;
+    ArithmeticAvg: Real = 0;
+    GeometricAvg: Real = 0;
+    GoodFlag: Boolean = False;
+
+Begin
+    // output task
+    Writeln('  The program is proving that arithmetic average of two unsigned');
+    Writeln('double numbers bigger or equal than geometric average of this numbers.');
+    Writeln('');
+    Repeat
+        Try
+            Begin
+                // input in console
+                Writeln('Enter first unsigned double number:');
+                Readln(Num1);
+                If Num1 < 0 Then
+                    Raise Exception.Create
+                        ('Entering number cannot be less than 1!!!');
+                Writeln('Enter second unsigned double number:');
+                Readln(Num2);
+                If Num2 < 0 Then
+                    Raise Exception.Create
+                        ('Entering number cannot be less than 1!!!');
+                // main block
+                ArithmeticAvg := (Num1 + Num2) / 2;
+                GeometricAvg := Sqrt(Num1 * Num2);
+                // ouput console
+                Writeln('');
+                Writeln('Arithmetic average: ' + FormatFloat('0.####',
+                    ArithmeticAvg));
+                Writeln('Geometric average: ' + FormatFloat('0.####',
+                    GeometricAvg));
+                If ArithmeticAvg = GeometricAvg Then
+                    Writeln('Arithmetic average = Geometric average')
+                Else
+                    Writeln('Arithmetic average > Geometric average');
+                GoodFlag := True;
+            End;
+        Except
+            On E: Exception Do
+            Begin
+                Write(E.Message);
+                Writeln(' Try again.');
+                Writeln('');
+                GoodFlag := False;
+            End;
+        End;
+    Until (GoodFlag);
+    // freeze console
+    Writeln('Press Enter to exit...');
+    Readln;
+End.
---
+++ Laboratory1/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+//main class
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //initialization
+        double num1 = 0, num2 = 0, arithmeticAvg = 0, geometricAvg = 0;
+        boolean badFlag = false;
+        //output task
+        System.out.println("  The program is proving that arithmetic average of two unsigned \n"
+            + "double numbers bigger or equal than geometric average of this numbers.\n");
+        do {
+            try
+            {
+                //input
+                System.out.println("Enter first unsigned double number:");
+                Scanner in = new Scanner(System.in);
+                num1 = in.nextDouble();
+                if (num1 < 0)
+                    throw new Exception("It's impossible to solve this exercise with num1 < 0!!!");
+                System.out.println("Enter second unsigned double number:");
+                num2 = in.nextDouble();
+                if (num2 < 0)
+                    throw new Exception("It's impossible to solve this exercise with num2 < 0!!!");
+                //main block
+                arithmeticAvg = (num1 + num2) / 2;
+                geometricAvg = Math.sqrt(num1 * num2);
+                //output
+                System.out.printf("""
+                        Arithmetic average: %f
+                        Geometric average: %f
+                        """, arithmeticAvg,geometricAvg);
+                if (arithmeticAvg == geometricAvg)
+                    System.out.println("Arithmetic average = Geometric average");
+                else
+                    System.out.println("Arithmetic average > Geometric average");
+                badFlag = false;
+            }
+            catch (Exception ex)
+            {
+                if (ex.getMessage() == null)
+                    System.out.println("Invalid type!!!" + " Try again.");
+                else
+                    System.out.println(ex.getMessage() + " Try again.");
+                badFlag = true;
+            }
+        } while (badFlag);
+    }
+}
