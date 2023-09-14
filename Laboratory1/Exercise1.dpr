03526238ecce38b14fc8a68947fc5cd94865b304
Aleeg0 <83497083+Aleeg0@users.noreply.github.com>
2023-09-14 19:37:36 +0300

    Exercise1.dpr

--- Laboratory1/Exercise1.dpr
+++ Laboratory1/Exercise1.dpr
@@ -12,10 +12,11 @@ Var
     GoodFlag: Boolean = False;
 
 Begin
-    // output task
+    // output the task
     Writeln('  The program is proving that arithmetic average of two unsigned');
     Writeln('double numbers bigger or equal than geometric average of this numbers.');
-    Writeln('');
+    Writeln;
+    // loop for check inputted symbols
     Repeat
         Try
             Begin
@@ -30,31 +31,31 @@ Begin
                 If Num2 < 0 Then
                     Raise Exception.Create
                         ('Entering number cannot be less than 1!!!');
-                // main block
-                ArithmeticAvg := (Num1 + Num2) / 2;
-                GeometricAvg := Sqrt(Num1 * Num2);
-                // ouput console
-                Writeln('');
-                Writeln('Arithmetic average: ' + FormatFloat('0.####',
-                    ArithmeticAvg));
-                Writeln('Geometric average: ' + FormatFloat('0.####',
-                    GeometricAvg));
-                If ArithmeticAvg = GeometricAvg Then
-                    Writeln('Arithmetic average = Geometric average')
-                Else
-                    Writeln('Arithmetic average > Geometric average');
+                // to exit the loop if user entered correct symbols
                 GoodFlag := True;
             End;
         Except
             On E: Exception Do
-                Begin
-                    Write(E.Message);
-                    Writeln(' Try again.');
-                    Writeln('');
-                End;
+            Begin
+                Write(E.Message);
+                Writeln(' Try again.');
+                Writeln;
+            End;
         End;
     Until (GoodFlag);
+    // main block
+    ArithmeticAvg := (Num1 + Num2) / 2;
+    GeometricAvg := Sqrt(Num1 * Num2);
+    // ouput console
+    Writeln;
+    Writeln('Arithmetic average: ', ArithmeticAvg:1:4);
+    Writeln('Geometric average: ', GeometricAvg:1:4);
+    If ArithmeticAvg = GeometricAvg Then
+        Writeln('Arithmetic average = Geometric average')
+    Else
+        Writeln('Arithmetic average > Geometric average');
     // freeze console
     Writeln('Press Enter to exit...');
     Readln;
+
 End.
