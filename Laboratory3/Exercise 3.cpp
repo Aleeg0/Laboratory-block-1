8e5f26e96a667eb10c663851c958b711b0f6f3fc
Aleeg0 <83497083+Aleeg0@users.noreply.github.com>
2023-09-18 18:43:17 +0300

    Update Exercise 3.cpp

--- Laboratory3/Exercise 3.cpp
+++ Laboratory3/Exercise 3.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 #include<math.h>
-#include<iomanip> //for std::setw() and std::left nad std::right
+#include<iomanip> // for std::setw() and std::left nad std::right
 
 
 int main()
 {
-    //initialization
+    // initialization
     int counter = 0; //for input
-    double eps = 0, xLast = 0, x = 0;
+    double eps = 0, xLast = 0, x = 0;// xLast is x0
     bool isFinding = false, // for task
         goodFlag = false; // for exit loop
-    //output the task
+    // output the task
     std::cout << "Program solves this example: Ln(7.622x) - 8.59x + 5 = 0.\n"
               << "It's recommended to input epsilon, using standard form.\n"
               << "Example: 1e-9\n"
@@ -38,7 +38,7 @@ int main()
         }
         catch (const std::exception& ex)
         {
-            std::cout << ex.what() << " Try again!!!\n";
+            std::cout << ex.what() << " Try again.\n";
         }
     } while (!goodFlag);
     // output some more info
@@ -58,41 +58,38 @@ int main()
                 throw std::exception("Invalid type!!!");
             }
             if (xLast < 0 || xLast == 0)
-                throw std::exception("X0 cannot be less or equal than 0, because in the task there's logarithm(7.622x > 0)!!!");
+                throw std::exception("X0 cannot be less or equal than 0, because in the task there's the logarithm!!!(7.622x > 0)");
+            // make sure that we can solve this task with current X0
+            if ((double)100 / ((double)859 * xLast) > 1) 
+                throw std::exception("The problem is not solvable using the inputted X0!!!");
             goodFlag = true;// to exit the loop if user entered correct limits
         }
         catch (const std::exception& ex)
         {
-            std::cout << ex.what() << " Try again!!!\n";
+            std::cout << ex.what() << " Try again.\n";
         }
     } while (!goodFlag);
     // main block
-    if ((double)100 / ((double)859 * xLast) >= 1)
+    // creating table
+    std::cout << std::endl << std::setw(6) << std::left << "   n"
+                << std::setw(13) << std::left << "|     x-1" 
+                << "|     x" << std::endl
+                << "--------------------------------\n";
+    while (!isFinding)
     {
-        std::cout << "The problem is not solvable using the inputted X0!!!\n";
+        x = (log(7.622 * xLast) + 5) / 8.59;
+        // output
+        std::cout << std::setw(5) << std::left << counter++ << " | "
+                    << std::setw(10) << std::left << xLast << " | "
+                    << x << std::endl;
+        // if we find the right X
+        if (abs(x - xLast) < eps)
+            isFinding = true;
+        else
+            xLast = x;
     }
-    else
-    {
-        //output
-        // creating table
-        std::cout << std::endl << std::setw(6) << std::left << "   n"
-                  << std::setw(13) << std::left << "|     x-1" 
-                  << "|     x" << std::endl
-                  << "--------------------------------\n";
-        while (!isFinding)
-        {
-            x = (log(7.622 * xLast) + 5) / 8.59;
-            std::cout << std::setw(5) << std::left << counter++ << " | "
-                      << std::setw(10) << std::left << xLast << " | "
-                      << x << std::endl;
-            if (abs(x - xLast) < eps)
-                isFinding = true;
-            else
-                xLast = x;
-        }
-        //output
-        std::cout << "\nThe answer of this task is "
-                  << x << std::endl;
-    }
+    // output
+    std::cout << "\nThe answer of this task is "
+                << x << std::endl;
     return 0;
 }
