83b4ad9438a09420e1d4ec444ec942d516dc6df0
Aleeg0 <83497083+Aleeg0@users.noreply.github.com>
2023-09-20 17:55:11 +0300

    Update Exercise2.dpr

--- Laboratory2/Exercise2.dpr
+++ Laboratory2/Exercise2.dpr
@@ -1,54 +1,89 @@
 Program Exercise2;
 
+Program Exercise3;
+
 Uses
     System.SysUtils;
 
-// initialization
 Var
-    N: Integer = 0;
-    Sum: Integer = 0;
-    NumberInLoop: Integer = 1;
-    // Low numbers for loops
-    I: Integer;
-    // Flag for end loop for inputted symbols
-    GoodFlag: Boolean = False;
+    Counter: Integer;
+    Eps: Real;
+    X: Real;
+    XLast: Real;
+    IsFinding: Boolean; // for task
+    GoodFlag: Boolean; // for exit loop
 
 Begin
+    // initialization
+    Counter := 0;
+    Eps := 0.0;
+    XLast := 0.0;
+    IsFinding := False;
+    GoodFlag := False;
     // output the task
-    Writeln('Calculate the sum using the formula (-1)^i * 2^i:');
-    Writeln;
-    // loop for check inputted symbols
+    Writeln('Program solves this example: Ln(7.622x) - 8.59x + 5.');
+    Writeln('It is recommended to input epsilon in standard form.');
+    Writeln('Example: 1e-9');
+    Writeln('Or using normal input.');
+    Writeln('Example: 0.1');
+    // loop for checking inputted eps
     Repeat
         Try
-            // input
-            Writeln('Enter n:');
-            Readln(N);
-            If N < 1 Then
-                Raise Exception.Create('N cannot be less than 1');
-            // to exit the loop if user entered correct symbols
+            Writeln('Enter eps:');
+            Readln(Eps);
+            If Eps > 0.1 Then
+                Raise EInOutError.Create('Epsilon is too big!!!');
+            If Eps < 1.0E-17 Then
+                Raise EInOutError.Create('Epsilon is too low!!!');
             GoodFlag := True;
         Except
-            On E: Exception Do
+            On E: EInOutError Do
             Begin
                 Write(E.Message);
-                Writeln('!!! Try again.');
+                Writeln(' Try again.');
             End;
         End;
-    Until (GoodFlag);
-    // main block
-    For I := 1 To N Do
+    Until GoodFlag;
+    // output some more info
+    Writeln('To start program also needs to input X0:');
+    // loop for checking inputted X0
+    GoodFlag := False;
+    Repeat
+        Try
+            Writeln('Enter X0:');
+            Readln(XLast); // x0
+            If (XLast < 0) Or (XLast = 0) Then
+                Raise EInOutError.Create
+                    ('X0 cannot be less or equal than 0, because in the task there is logarithm(7.622x > 0)!!!');
+            If 100 / (859 * XLast) > 1 Then
+                Raise EInOutError.Create
+                    ('The problem is not solvable using the inputted X0!!!');
+            GoodFlag := True;
+        Except
+            On E: EInOutError Do
+            Begin
+                Write(E.Message);
+                Writeln(' Try again.');
+            End;
+        End;
+    Until GoodFlag;
+    Writeln(#9, 'N', #9, '|     X - 1', #9, '|', #9, 'X');
+    Writeln('-------------------------------------------------');
+    While IsFinding = False Do
     Begin
-        NumberInLoop := NumberInLoop * 2;
-        If I Mod 2 <> 0 Then
-            Sum := Sum - NumberInLoop
+        X := (Ln(7.662 * XLast) + 5) / 8.59;
+        Writeln(Counter:9, '|':8, XLast:11:5, #9, '|', X:11:5);
+        If Abs(X - XLast) < Eps Then
+            IsFinding := True
         Else
-            Sum := Sum + NumberInLoop;
+            XLast := X;
+        Counter := Counter + 1;
     End;
     // output
-    Writeln('Sum equal ', Sum);
     Writeln;
-    // End of program
-    Writeln('Press Enter to exit...');
+    Writeln('The answer of this task is ', X:6:5);
+    // freeze console
+    Writeln('Press enter to exit...');
     Readln;
 
 End.
